6a82bfc698fbcae43638da9249e63578d9a3f924
THEMANJH <jonghwan2208@inha.edu>
2025-07-12 15:26:27 +0900

    최종 앱 코드 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
 // app/layout.tsx
+import type { Metadata } from "next";
+import "./globals.css";
 
-// 이 파일은 모든 페이지의 기본 뼈대가 됩니다.
+export const metadata: Metadata = {
+  title: "오운완 - 오늘 운동 완료",
+  description: "나만의 운동을 기록하고 성장하세요.",
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="ko">
-      <body>
-        <h1>여기는 레이아웃입니다.</h1>
-        <hr />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
